43a5dbb85423dd96f6025b267dc0cb5ea2c9d18b
Ayush Raman <54257595+ayuram@users.noreply.github.com>
2021-08-31 19:33:49 -0700

    Initial Commit

---
+++ Shared/contributrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  contributrackApp.swift
+//  Shared
+//
+//  Created by Ayush Raman on 8/31/21.
+//
+
+import SwiftUI
+
+@main
+struct contributrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
