db76bc448d4e68734155d37fb352ce86a9e6d1cd
Ayush Raman <54257595+ayuram@users.noreply.github.com>
2021-09-02 17:11:44 -0700

    backend

--- Shared/backend.swift
+++ Shared/backend.swift
@@ -10,7 +10,61 @@ import Foundation
 
 struct Unit {
     let notation: String
+    let name: String
     let prefixed: Bool
     var increment: Double
     var decrement: Double
+}
+
+enum UserType {
+    case lead, member
+}
+
+struct User : Hashable {
+    static func == (lhs: User, rhs: User) -> Bool {
+        lhs.uid == rhs.uid
+    }
+    let uid: String
+    let pfp: String?
+    var name: String
+    var type: UserType
+    init(uid: String? = .none, pfp: String? = .none, name: String, type: UserType) {
+        self.uid = uid ?? UUID().uuidString
+        self.pfp = pfp
+        self.name = name
+        self.type = type
+    }
+    init() {
+        self.uid = UUID().uuidString
+        self.pfp = .none
+        self.name = ""
+        self.type = UserType.member
+    }
+}
+
+
+
+class Workspace {
+    private var name: String
+    private var unit: Unit
+    private var users = [User:Double]()
+    init(withUnits unit: Unit, withUsers users: [User]? = .none, name: String) {
+        self.unit = unit
+        self.name = name
+        for user in users ?? [] {
+            self.users[user] = 0.0
+        }
+    }
+    func addUser(user: User) {
+        guard user.type == .member else {
+            return
+        }
+        users[user] = 0.0
+    }
+    func getTopUser() -> User? {
+        users.max { maxUser, currUser in currUser.value < maxUser.value }?.key
+    }
+    func getStatistic(fun operation: ([Double]) -> Double?) -> Double? {
+        operation(users.values.sorted())
+    }
 }
